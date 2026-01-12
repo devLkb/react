d965d908342a17e5af78b61f3976c78278b59460
LKB <ggubo01@gmail.com>
2026-01-12 17:10:05 +0900

    latest

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './Hello';
 
+const Welcome = (props) => {return (<h1> 안녕, {props.name} </h1>);};
+const element = <Welcome name="iceman"/>;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -14,4 +17,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
